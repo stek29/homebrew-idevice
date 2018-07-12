9b9cda46e245424a8233009708d221df82386e53
Viktor Oreshkin <imselfish@stek29.rocks>
2018-07-12 21:55:53 +0300

    Update iokit-utils

--- Formula/iokit-utils.rb
+++ Formula/iokit-utils.rb
@@ -1,8 +1,9 @@
 class IokitUtils < Formula
   desc "Dev tools for probing IOKit"
   homepage "https://github.com/Siguza/iokit-utils"
-  url "https://github.com/Siguza/iokit-utils.git"
-  version "git0"
+  url "https://github.com/Siguza/iokit-utils.git",
+    tag: "1.2.3",
+    revision: "71a84147c595388a49d40de6bfbadb0aa78790cf"
 
   def install
     system "make"
