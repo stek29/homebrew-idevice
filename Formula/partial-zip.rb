644f77343e8a882ca0762e7c418b8688e0864360
Viktor Oreshkin <imselfish@stek29.rocks>
2018-09-02 10:49:25 +0300

    Fix bug in partial-zip, bump revision

--- Formula/partial-zip.rb
+++ Formula/partial-zip.rb
@@ -1,9 +1,10 @@
 class PartialZip < Formula
-  desc "tool for image4 and der"
-  homepage "https://github.com/xerub/img4tool"
+  desc "retrieve selected portions of ZIP files"
+  homepage "https://github.com/planetbeing/partial-zip"
 
   url "https://github.com/planetbeing/partial-zip.git"
   version "git0"
+  revision 1
 
   depends_on "cmake" => :build
   depends_on "zlib"
@@ -19,7 +20,6 @@ end
 
 __END__
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index fa44fe3b1460..a5e7ee4697bc 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
 @@ -32,2 +32,5 @@
@@ -28,3 +28,12 @@ index fa44fe3b1460..a5e7ee4697bc 100644
 +install(TARGETS partialzip DESTINATION bin)
 +install(TARGETS partial DESTINATION lib)
 +install(FILES include/partial/partial.h DESTINATION include)
+
+diff --git a/main.c b/main.c
+--- a/main.c
++++ b/main.c
+@@ -16,3 +16,4 @@
+	char* extract = argv[2];
+        char fname[strlen(argv[1]) + sizeof("file://")];
++        fname[0] = '\0';
+	char* outfile = argv[2];
