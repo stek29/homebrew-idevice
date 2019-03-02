e83afa2d57d1522a8f91df572e6d0747deea2615
Viktor Oreshkin <imselfish@stek29.rocks>
2019-03-03 00:10:29 +0300

    update radare2 (3.3.0)

--- Casks/radare2.rb
+++ Casks/radare2.rb
@@ -1,6 +1,6 @@
 cask 'radare2' do
-  version '3.2.1'
-  sha256 '936f66d1197d341c1b7c715ece77f9aca860e4e96efe7c61f8ac5b66f3e7dfcd'
+  version '3.3.0'
+  sha256 '65c973bdefcfb8fc368daadf9f53b2b2f26305be054f7c8525ca2b2bd7c29e40'
 
   # radare.mikelloc.com was verified as official when first introduced to the cask
   url "http://radare.mikelloc.com/get/#{version}/radare2-#{version}.pkg"
