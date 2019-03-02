1df27c396b3d0209886f644b5405c1dbfa72347d
Viktor Oreshkin <imselfish@stek29.rocks>
2019-03-03 00:03:19 +0300

    Update genlist.sh

--- genlist.sh
+++ genlist.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
-cd $(dirname $0)/Formula && (for f in *; printf '- [%s]( %s )\n' "${f/.rb/}" "$(gsed -n 's/^.*homepage "\(.*\)"/\1/p' $f)") | sort
+cd $(dirname $0)/Formula
+echo "## Formulas in this tap"
+(for f in *; do
+  printf -- '- [%s]( %s ) -- version %s\n' "${f/.rb/}" "$(gsed -n 's/^.*homepage "\(.*\)"/\1/p' $f)" "$(gsed -n 's/^.*version "\(.*\)"/\1/p' $f)" | sed 's/-- version \(\|git0\)$//'
+done
+) | sort
 
