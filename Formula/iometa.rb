0ebd1302a4e7824cd0db6043317caa85e7b9c66b
Viktor Oreshkin <imselfish@stek29.rocks>
2019-04-21 00:35:39 +0300

    Update iometa (1.3.0)

--- Formula/iometa.rb
+++ Formula/iometa.rb
@@ -2,7 +2,7 @@ class Iometa < Formula
   desc "Just another IOKit class dumper"
   homepage "https://github.com/Siguza/iometa"
   url "https://github.com/Siguza/iometa.git"
-  version "git0"
+  version "1.3.0"
 
   def install
     system "make"
