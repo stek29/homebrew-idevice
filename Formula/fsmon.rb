7fc06b9c3c7f46b8ca4c2da06432b3078a37f620
Viktor Oreshkin <imselfish@stek29.rocks>
2019-04-21 00:50:25 +0300

    Update fsmon (1.6.1)

--- Formula/fsmon.rb
+++ Formula/fsmon.rb
@@ -1,7 +1,7 @@
 class Fsmon < Formula
   desc "FileSystem events Monitor"
   homepage "https://github.com/nowsecure/fsmon"
-  url "https://github.com/nowsecure/fsmon/archive/1.5.tar.gz"
+  url "https://github.com/nowsecure/fsmon/archive/1.6.1.tar.gz"
   sha256 "37ea1c83297976f5c7058637a328150dea57743d5eb55ebfc3a8075d262d67c2"
 
   def install
