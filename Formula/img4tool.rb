fe5e591dbc8db2f8e48a71307d1bcabe7fa686ba
Viktor Oreshkin <imselfish@stek29.rocks>
2019-08-10 05:29:32 +0300

    Update img4tool (122)

--- Formula/img4tool.rb
+++ Formula/img4tool.rb
@@ -2,8 +2,8 @@ class Img4tool < Formula
   desc "Tool for manipulating IMG4, IM4M and IM4P files"
   homepage "https://github.com/tihmstar/img4tool"
   url "https://github.com/tihmstar/img4tool.git",
-    :revision => "1bffa0e699602b4926e3a2d15e5217fba3ecb6f5"
-  version "118"
+    :revision => "29f25cb4053376b83393a7f365e15767e75d3fab"
+  version "122"
   head "https://github.com/tihmstar/img4tool.git"
 
   depends_on "autoconf" => :build
