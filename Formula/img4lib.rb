28d7407fe1b79ab15e3d3173d8ab5520e9fee69c
Viktor Oreshkin <imselfish@stek29.rocks>
2020-05-11 22:35:04 +0300

    update img4lib (ed6a796)

--- Formula/img4lib.rb
+++ Formula/img4lib.rb
@@ -3,8 +3,8 @@ class Img4lib < Formula
   homepage "https://github.com/xerub/img4lib"
 
   url "https://github.com/xerub/img4lib.git",
-    :revision => "872230dbb1edfe2167c9db516db9f5e55cf48ba9"
-  version "1.git-19"
+    :revision => "ed6a79670cf4460488d83702019e7bde7577032f"
+  version "1.git-31"
 
   head "https://github.com/xerub/img4lib.git"
 
