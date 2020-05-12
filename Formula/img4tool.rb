d92efcce42ccc60340f5c3d20719def626bcfebc
Viktor Oreshkin <imselfish@stek29.rocks>
2020-05-12 03:01:30 +0300

    update img4tool (182)

--- Formula/img4tool.rb
+++ Formula/img4tool.rb
@@ -2,8 +2,7 @@ class Img4tool < Formula
   desc "Tool for manipulating IMG4, IM4M and IM4P files"
   homepage "https://github.com/tihmstar/img4tool"
   url "https://github.com/tihmstar/img4tool.git",
-    :revision => "29f25cb4053376b83393a7f365e15767e75d3fab"
-  version "122"
+    :tag => "182"
   head "https://github.com/tihmstar/img4tool.git"
 
   depends_on "autoconf" => :build
@@ -12,17 +11,11 @@ class Img4tool < Formula
   depends_on "pkg-config" => :build
 
   depends_on "libplist"
-  depends_on "openssl"
+  depends_on "libtihmstargeneral"
+  depends_on "openssl@1.1"
 
   def fix_tihmstar
-    if File.symlink?("COPYING")
-      rm "COPYING"
-      touch "LICENSE"
-      cp "LICENSE", "COPYING"
-    end
-
-    files = %w[setBuildVersion.sh autogen.sh configure.ac]
-    inreplace files.select { |f| File.exist? f },
+    inreplace %w[configure.ac],
       "$(git rev-list --count HEAD)",
       version.to_s.gsub(/[^\d]/, ""),
       false
@@ -35,6 +28,7 @@ class Img4tool < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
+
     system "make"
     bin.install "img4tool/img4tool"
   end
