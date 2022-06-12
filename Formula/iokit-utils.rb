944eefd881ddb04b62eaf086161ffcaf7f68e7b4
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:20:04 +0300

    iokit-utils: rebuild
    reword iokit-utils

--- Formula/iokit-utils.rb
+++ Formula/iokit-utils.rb
@@ -1,5 +1,5 @@
 class IokitUtils < Formula
-  desc "Dev tools for probing IOKit"
+  desc "Little dev tools for probing IOKit"
   homepage "https://github.com/Siguza/iokit-utils"
   url "https://github.com/Siguza/iokit-utils/archive/1.3.0.tar.gz"
   sha256 "9df7dbb0ab78425d82370f87dffaa25161aa79a9ab13b1bc39797f03731563d9"
