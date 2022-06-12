751c9809568c3da9254e09dbc7411afa52594718
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 22:33:25 +0000

    ios-kern-utils: add 1.4.1 bottle.

--- Formula/ios-kern-utils.rb
+++ Formula/ios-kern-utils.rb
@@ -6,6 +6,14 @@ class IosKernUtils < Formula
 
   head "https://github.com/Siguza/ios-kern-utils.git"
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "ee4ce3a58d895026c21caf4b30682dde636613a6d02c7815e9a7e1faed3ef241"
+    sha256 cellar: :any_skip_relocation, big_sur:  "4130425f20828e8fae21b93230a9e55f2d162d49f8be69cf94accb278ec5c6ea"
+    sha256 cellar: :any_skip_relocation, catalina: "dded795f5f2fa031258162dccbe42d5baf2448dc27cca0104b6d845311aa7bee"
+  end
+
   def install
     system "make"
 
