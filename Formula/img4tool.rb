feda1e7b6470c854c68d6edbbf32e118b1e0172e
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 18:03:40 +0300

    update and livecheck img4tool

--- Formula/img4tool.rb
+++ Formula/img4tool.rb
@@ -2,8 +2,15 @@ class Img4tool < Formula
   desc "Tool for manipulating IMG4, IM4M and IM4P files"
   homepage "https://github.com/tihmstar/img4tool"
   url "https://github.com/tihmstar/img4tool.git",
-    :tag => "195"
+    :revision => "aca6cf005c94caf135023263cbb5c61a0081804f"
+  version "197"
   head "https://github.com/tihmstar/img4tool.git"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/<strong>(\d+)<\/strong>\s*<span aria-label="Commits/m)
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
