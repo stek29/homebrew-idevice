66cafca8b0045d34bde1ebff3a50515ca6ec477f
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 23:51:23 +0000

    ldid2: add 42-3-gdb74fea bottle.

--- Formula/ldid2.rb
+++ Formula/ldid2.rb
@@ -12,6 +12,14 @@ class Ldid2 < Formula
     skip "upstream has inconsistent tags"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "610074df54c12ccbea374e4e21c3493c30f5de5f9921b2b8e8aebf0662887d7c"
+    sha256 cellar: :any_skip_relocation, big_sur:  "1f35dfd048633859e193456667c6803aefb1de912be4119e64d399e4be830a52"
+    sha256 cellar: :any_skip_relocation, catalina: "09fb4bf2fbc3b42a090cd6913669f468c70e0b02377cab15efcc92cd87a35f35"
+  end
+
   depends_on "openssl@1.1"
 
   def install
