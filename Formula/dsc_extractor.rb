056d10216902314b8e1302494be776f490febc02
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 00:05:36 +0300

    style fixes

--- Formula/dsc_extractor.rb
+++ Formula/dsc_extractor.rb
@@ -4,20 +4,20 @@ class DscExtractor < Formula
   url "https://opensource.apple.com/tarballs/dyld/dyld-852.2.tar.gz"
   sha256 "f38e62fa9d3aec0bbc487cc27bf141b060e163195878c2dde31cb54609e436c4"
 
-  depends_on :xcode
-
   livecheck do
     url :homepage
     strategy :page_match
-    regex(/a href=dyld-([\d.]+).tar.gz/m)
+    regex(/a href=dyld-([\d.]+)\.t/im)
   end
+
+  depends_on :xcode
 
   def install
     src = "dyld3/shared-cache/dsc_extractor.cpp"
     inreplace src,
-      %r{.*\n#if 0}m,
+      /.*\n#if 0/m,
       "#if 1"
-    system "#{ENV.cc}", "-o", "dsc_extractor", src
+    system ENV.cc.to_s, "-o", "dsc_extractor", src
     bin.install "dsc_extractor"
   end
 end
