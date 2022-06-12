cdf54659004b97486e35d21bef840cfc80c16fb4
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 19:13:41 +0300

    fix libirecovery build deps

--- Formula/libirecovery.rb
+++ Formula/libirecovery.rb
@@ -12,6 +12,7 @@ class Libirecovery < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "libusb"
   depends_on "libimobiledevice-glue"
