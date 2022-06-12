50b6c637593ff43097060af6dd8704b99efaca1b
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:22:19 +0300

    reword iometa

--- Formula/iometa.rb
+++ Formula/iometa.rb
@@ -1,5 +1,5 @@
 class Iometa < Formula
-  desc "Just another IOKit class dumper"
+  desc "Extracts C++ class runtime information from arm64 Darwin kernel"
   homepage "https://github.com/Siguza/iometa"
   url "https://github.com/Siguza/iometa/archive/1.6.6.tar.gz"
   sha256 "147c88729d897e78688ab969ffb009426d9f1e21895bab6e4e54baed4699dd30"
