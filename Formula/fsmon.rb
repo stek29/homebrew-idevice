dd502be1fb35a8c40b59835455ad3c86d250cfc1
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 21:52:22 +0000

    fsmon: add 1.8.5 bottle.

--- Formula/fsmon.rb
+++ Formula/fsmon.rb
@@ -4,6 +4,14 @@ class Fsmon < Formula
   url "https://github.com/nowsecure/fsmon/archive/1.8.5.tar.gz"
   sha256 "eb99cfb242bea9fc5bde66e67f4324bd71100d17b1672e4e52db14b9a5e2900a"
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "d079dbdf4f4c66e143a2ed736e2bc0333dfa2f14e1cff4fbd2db37784ecb09f7"
+    sha256 cellar: :any_skip_relocation, big_sur:  "30e4c0964f1eeb268cc7c0339650880dbae62f3a199852df6ae5afb92fbf9afc"
+    sha256 cellar: :any_skip_relocation, catalina: "ac6ddf44462b2cd33c6b2927a5b2cd998a1e543427e3b3f81e6834d5370f64e2"
+  end
+
   def install
     system "make", "macos"
     mv "fsmon-macos", "fsmon"
