ef082376577553676fe38c5a61d8df7952e84fa5
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:04:26 +0300

    libimobiledevice-glue: rebuild
    * reword libimobiledevice-glue
    * libimobiledevice: add pkg-config dep
    
    Closes #29.
    
    Signed-off-by: Viktor Oreshkin <imselfish@stek29.rocks>

--- Formula/libimobiledevice-glue.rb
+++ Formula/libimobiledevice-glue.rb
@@ -1,5 +1,5 @@
 class LibimobiledeviceGlue < Formula
-  desc "Library with common code used by the libimobiledevice project"
+  desc "Common code used by the libimobiledevice project"
   homepage "https://github.com/libimobiledevice/libimobiledevice-glue"
   url "https://github.com/libimobiledevice/libimobiledevice-glue.git",
     revision: "d2ff7969dcd0a12e4f18f63dab03e6cd03054fcb"
@@ -10,11 +10,12 @@ class LibimobiledeviceGlue < Formula
     skip "upstream has inconsistent tags"
   end
 
-  keg_only "its an utility library for libimobiledevice"
+  keg_only "it's an utility library for libimobiledevice"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "stek29/idevice/libplist"
 
