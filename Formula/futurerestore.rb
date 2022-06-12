e6470a573ce707faae49aca5dd24a2c85da5c828
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 23:06:25 +0300

    disable futurerestore

--- Formula/futurerestore.rb
+++ Formula/futurerestore.rb
@@ -1,11 +1,12 @@
 class Futurerestore < Formula
   desc "Hacked up idevicerestore wrapper"
-  homepage "https://github.com/tihmstar/futurerestore"
-  url "https://github.com/tihmstar/futurerestore.git",
-    revision: "2f0686d3559f1543fc0c0c741b68e1ae940c5a30"
-  version "186"
+  disable! date: "2022-06-11", because: "cant be built - see https://github.com/stek29/homebrew-idevice/issues/23"
+  homepage "https://github.com/futurerestore/futurerestore"
+  url "https://github.com/futurerestore/futurerestore.git",
+    revision: "f7fa07e2bae25b333f2365b06c23a461ed022a5a"
+  version "279"
 
-  head "https://github.com/tihmstar/futurerestore.git"
+  head "https://github.com/futurerestore/futurerestore.git"
 
   livecheck do
     url :homepage
@@ -19,39 +20,23 @@ class Futurerestore < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
 
   depends_on "img4tool"
   depends_on "libfragmentzip"
-  depends_on "libimobiledevice"
-  depends_on "libirecovery"
-  depends_on "libplist"
   depends_on "libzip"
-  depends_on "libipatcher" => :optional
 
-  def fix_tihmstar
-    File.symlink "LICENSE", "COPYING"
-
-    inreplace %w[configure.ac],
-      "git rev-list --count HEAD",
-      "echo #{version.to_s.gsub(/[^\d]/, "")}",
-      false
-  end
+  depends_on "tsschecker"
+  depends_on "stek29/idevice/libimobiledevice"
+  depends_on "stek29/idevice/libirecovery"
+  depends_on "stek29/idevice/libplist"
 
   def install
-    fix_tihmstar
+    ENV["ARCH"] = "x86_64"
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
 
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --prefix=#{prefix}
-    ]
-    args << "--without-libipatcher" if build.without? "libipatcher"
-    system "./autogen.sh", *args
-    system "make"
-
-    # we don't need libjssy.a and idevicerestore.1
-    # system "make", "install"
-    bin.install "futurerestore/futurerestore" => "futurerestore"
+    # we don't need libjssy.a
+    bin.install "src/futurerestore" => "futurerestore"
   end
 end
