b3e4c567547b37c0131c6e5396636721c91da76e
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 02:27:40 +0300

    unsign: rebuild
    * unsign: add license - see #32
    * unsign: fix livecheck and homepage
    
    Closes #36.
    
    Signed-off-by: Viktor Oreshkin <imselfish@stek29.rocks>

--- Formula/unsign.rb
+++ Formula/unsign.rb
@@ -1,14 +1,15 @@
 class Unsign < Formula
-  desc "Remove code signatures from OSX Mach-O binaries"
-  homepage "http://www.woodmann.com/collaborative/tools/index.php/Unsign"
+  desc "Remove code signatures from Mach-O binaries"
+  homepage "https://web.archive.org/web/20200214131522/http://www.woodmann.com/collaborative/tools/index.php/Unsign"
   url "https://github.com/steakknife/unsign.git",
     revision: "37151232e3a3186dc60a33c920c60044aa044788"
   version "0.11"
+  license "ISC"
   head "https://github.com/steakknife/unsign.git"
 
   livecheck do
     url :url
-    regex(%r{<strong>(\d+)</strong>\s*<spanaria-label="Commits}im)
+    regex(%r{<strong>(\d+)</strong>\s*<span aria-label="Commits}im)
     strategy :page_match do |page|
       v = page.match(regex)&.captures&.first
       "0.#{v}" if v
