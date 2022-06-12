2de72577fde88f211c2f6cecc2d0d1f194ce9c67
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 18:04:52 +0300

    update libirecovery

--- Formula/libirecovery.rb
+++ Formula/libirecovery.rb
@@ -1,8 +1,13 @@
 class Libirecovery < Formula
   desc "Library for communication to iBoot/iBSS on iOS devices via USB"
   homepage "https://github.com/libimobiledevice/libirecovery"
-  url "https://github.com/libimobiledevice/libirecovery.git"
-  version "git0"
+  url "https://github.com/libimobiledevice/libirecovery.git",
+    :revision => "17c02beca2f99b27dab5c1f83cfe4e33036e462b"
+  version "1.0.1-46-g17c02be"
+
+  livecheck do
+    skip "upstream has inconsistent tags"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
