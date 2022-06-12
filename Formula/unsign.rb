a7f583a67b9d1aa6328c7f4c1fe59da549b45eec
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 18:02:02 +0300

    update and livecheck unsign

--- Formula/unsign.rb
+++ Formula/unsign.rb
@@ -2,9 +2,19 @@ class Unsign < Formula
   desc "Remove code signatures from OSX Mach-O binaries"
   homepage "http://www.woodmann.com/collaborative/tools/index.php/Unsign"
   url "https://github.com/steakknife/unsign.git",
-    :revision => "8cf356ca335d0709df3800e12ff07c25ec5ddd35"
-  version "0.10"
+    :revision => "37151232e3a3186dc60a33c920c60044aa044788"
+  version "0.11"
   head "https://github.com/steakknife/unsign.git"
+
+  livecheck do
+    url :url
+    strategy :page_match do |page|
+      v = page
+        .match(/<strong>(\d+)<\/strong>\s*<span aria-label="Commits/m)
+        &.captures&.first
+      "0.#{v}" if v
+    end
+  end
 
   def install
     system "make"
