27e43f7a4d94db2326573ffd11ef3758f3c76082
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 22:38:58 +0000

    libimobiledevice-glue: add 1.0.0-35-gd2ff796 bottle.

--- Formula/libimobiledevice-glue.rb
+++ Formula/libimobiledevice-glue.rb
@@ -10,6 +10,14 @@ class LibimobiledeviceGlue < Formula
     skip "upstream has inconsistent tags"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any, monterey: "1b217a06ea71672717a87df850c67ca59962442a01f603ba4ea8883edbf52d75"
+    sha256 cellar: :any, big_sur:  "309351efbaa9f40b2475673f411f769a515de36335c8350c0fd57b1a63f5bf93"
+    sha256 cellar: :any, catalina: "16d7b3449aa808b0f246c4be2a5210b385b68f77a2f74646b378cf768d27981b"
+  end
+
   keg_only "it's an utility library for libimobiledevice"
 
   depends_on "autoconf" => :build
