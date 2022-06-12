3de035d004f109374f158d5e756e731e3e5f264f
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:07:42 +0300

    libirecovery: rebuild
    * reword libirecovery
    * libirecovery: add head and license - see #32

--- Formula/libirecovery.rb
+++ Formula/libirecovery.rb
@@ -1,13 +1,17 @@
 class Libirecovery < Formula
-  desc "Library for communication to iBoot/iBSS on iOS devices via USB"
+  desc "Library for communication with iBoot/iBSS of iOS devices via USB"
   homepage "https://github.com/libimobiledevice/libirecovery"
   url "https://github.com/libimobiledevice/libirecovery.git",
     revision: "17c02beca2f99b27dab5c1f83cfe4e33036e462b"
   version "1.0.1-46-g17c02be"
+  license "LGPL-2.1-or-later"
+  head "https://github.com/libimobiledevice/libirecovery.git"
 
   livecheck do
     skip "upstream has inconsistent tags"
   end
+
+  keg_only "it can conflict with homebrew/core"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
