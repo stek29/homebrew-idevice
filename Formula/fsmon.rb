31b1f5fef1a7434bc721e1e97e36f7b7946cbe31
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 00:21:07 +0300

    fsmon: rebuild
    update fsmon description for tests
    
    Closes #27.
    
    Signed-off-by: Viktor Oreshkin <imselfish@stek29.rocks>

--- Formula/fsmon.rb
+++ Formula/fsmon.rb
@@ -1,5 +1,5 @@
 class Fsmon < Formula
-  desc "FileSystem events Monitor"
+  desc "FileSystem events monitor"
   homepage "https://github.com/nowsecure/fsmon"
   url "https://github.com/nowsecure/fsmon/archive/1.8.5.tar.gz"
   sha256 "eb99cfb242bea9fc5bde66e67f4324bd71100d17b1672e4e52db14b9a5e2900a"
