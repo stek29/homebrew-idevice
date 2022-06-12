744edb4712cc522da0b7a147821c905af8ec5c68
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:28:31 +0300

    libimobiledevice: add pkg-config dep

--- Formula/libimobiledevice-glue.rb
+++ Formula/libimobiledevice-glue.rb
@@ -15,6 +15,7 @@ class LibimobiledeviceGlue < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "stek29/idevice/libplist"
 
