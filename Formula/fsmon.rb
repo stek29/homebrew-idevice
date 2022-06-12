2d36c747aa1c837030a69705827a95f644ef6532
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 18:18:02 +0300

    fsmon: fix build, add test

--- Formula/fsmon.rb
+++ Formula/fsmon.rb
@@ -5,8 +5,12 @@ class Fsmon < Formula
   sha256 "eb99cfb242bea9fc5bde66e67f4324bd71100d17b1672e4e52db14b9a5e2900a"
 
   def install
-    system "make", "osx"
-    mv "fsmon-osx", "fsmon"
+    system "make", "macos"
+    mv "fsmon-macos", "fsmon"
     system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_equal "fsmon #{version}\n", shell_output("#{bin}/fsmon -v")
   end
 end
