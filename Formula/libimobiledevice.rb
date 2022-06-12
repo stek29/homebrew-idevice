8ea2ea515808329b0febd8ef6ce769463976f847
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-12 23:51:55 +0000

    libimobiledevice: add 1.3.0-158-g93c25b7 bottle.

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -11,6 +11,14 @@ class Libimobiledevice < Formula
     skip "upstream has inconsistent tags"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any, monterey: "7da19d95a57bb0cab25a791a561f07fd2e03b657114089add2e8f29f2bd11307"
+    sha256 cellar: :any, big_sur:  "0bd130bd38a83df2b47bd2cf6b194a9eace6c85692df297e7770f2be8035b28d"
+    sha256 cellar: :any, catalina: "408744b6dbb80fc7dd000c8a231205967407554189b075cd41139fd9b4680de1"
+  end
+
   keg_only "it can conflict with homebrew/core"
 
   depends_on "autoconf" => :build
