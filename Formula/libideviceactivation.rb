71b1400af11031556565ae75bfaa73efbc35e0fb
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 02:04:12 +0300

    libideviceactivation 1.1.2-12-g75505b7
    * ideviceactivation: add license - see #32
    * libideviceactivation: reword
    * libideviceactivation: correct version, add test

--- Formula/libideviceactivation.rb
+++ Formula/libideviceactivation.rb
@@ -1,10 +1,16 @@
 class Libideviceactivation < Formula
-  desc "Library handle the activation process of iOS devices"
+  desc "Library to handle the activation process of iOS devices"
   homepage "https://www.libimobiledevice.org/"
   url "https://github.com/libimobiledevice/libideviceactivation.git",
     revision: "75505b75816691e1ff651fb55bfc6aaa7170ba3f"
-  version "1.1.1-12-g75505b7"
 
+  # it's actually 1.1.1-12-g75505b7 if using git describe
+  # but 1.1.2 is hardcoded in libideviceactivation, unlike other libimobiledevice
+  # projects, which use git describe based versioning
+  # so 1.1.2 is from hardcoded version, and second part is from git describe
+  version "1.1.2-12-g75505b7"
+
+  license "LGPL-3.0-or-later"
   head "https://github.com/libimobiledevice/libideviceactivation.git"
 
   livecheck do
@@ -27,5 +33,9 @@ class Libideviceactivation < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_equal "ideviceactivation #{version.to_s.split("-").first}\n", shell_output("#{bin}/ideviceactivation -v")
   end
 end
