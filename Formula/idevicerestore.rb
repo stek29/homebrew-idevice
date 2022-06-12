c4642c16cb2501431a9d3b8e777f74a3c26967fe
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 01:08:55 +0300

    idevicerestore: rebuild
    * reword idevicerestore
    * idevicerestore: add test
    * idevicerestore: add license - see #32

--- Formula/idevicerestore.rb
+++ Formula/idevicerestore.rb
@@ -1,17 +1,17 @@
 class Idevicerestore < Formula
-  desc "Restore/upgrade firmware of iOS devices"
+  desc "Restore firmware files to iOS devices"
   homepage "https://www.libimobiledevice.org/"
   url "https://github.com/libimobiledevice/idevicerestore.git",
     revision: "f80a876b3598de4eb551bafcb279947c527fae33"
   version "1.0.0-95-gf80a876"
-
+  license "LGPL-3.0-or-later"
   head "https://github.com/libimobiledevice/idevicerestore.git"
 
   livecheck do
     skip "upstream has inconsistent tags"
   end
 
-  keg_only "to avoid conflicts with homebrew/core"
+  keg_only "it can conflict with homebrew/core"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -30,5 +30,9 @@ class Idevicerestore < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    assert_equal "idevicerestore #{version}\n", shell_output("#{bin}/idevicerestore -v")
   end
 end
