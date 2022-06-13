284df74b8f177d36304ff3378eee489ffe84273d
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 03:22:42 +0300

    dsc_extractor: fix dlopen call too
    Closes #39.
    
    Signed-off-by: Viktor Oreshkin <imselfish@stek29.rocks>

--- Formula/dsc_extractor.rb
+++ Formula/dsc_extractor.rb
@@ -3,6 +3,7 @@ class DscExtractor < Formula
   homepage "https://opensource.apple.com/tarballs/dyld/"
   url "https://opensource.apple.com/tarballs/dyld/dyld-852.2.tar.gz"
   sha256 "f38e62fa9d3aec0bbc487cc27bf141b060e163195878c2dde31cb54609e436c4"
+  revision 1
 
   livecheck do
     url :homepage
@@ -14,9 +15,11 @@ class DscExtractor < Formula
 
   def install
     src = "dyld3/shared-cache/dsc_extractor.cpp"
-    inreplace src,
-      /.*\n#if 0/m,
-      "#if 1"
+    inreplace src do |s|
+      s.gsub!(/.*\n#if 0/m, "#if 1")
+      s.gsub! %r{(dlopen\(").*/(dsc_extractor.bundle)}, '\1\2'
+    end
+
     system ENV.cxx.to_s, "-o", "dsc_extractor", src
     bin.install "dsc_extractor"
   end
