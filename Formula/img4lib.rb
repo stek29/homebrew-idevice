02c59965a30416193c9ab6fcae144a6a5e616a42
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 04:39:52 +0300

    img4lib: use libcompression and commoncrypto

--- Formula/img4lib.rb
+++ Formula/img4lib.rb
@@ -12,10 +12,32 @@ class Img4lib < Formula
     skip "upstream has no tags"
   end
 
-  depends_on "openssl@1.1"
+  # libcompression is provided since macOS 10.11
+  # according to https://developer.apple.com/documentation/compression
+  # on older versions lzfse is needed
+  on_macos do
+    depends_on "lzfse" if MacOS.version < :el_capitan
+  end
+
+  on_linux do
+    depends_on "lzfse"
+    depends_on "openssl@1.1"
+  end
 
   def install
-    system "make"
+    args = []
+    if OS.mac?
+      args << "COMMONCRYPTO=1"
+
+      if MacOS.version >= :el_capitan
+        # remove libcompression check – it's expected to be present on recent macOS versions
+        inreplace "Makefile",
+          "$(wildcard /usr/lib/libcompression.dylib)", "x",
+          false
+      end
+    end
+
+    system "make", *args
     bin.install "img4"
   end
 end
