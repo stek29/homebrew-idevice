cddd24d49ca6fb632bde1687c2cd59a7aa5fd603
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 00:05:04 +0000

    unsign: add 0.11 bottle.

--- Formula/unsign.rb
+++ Formula/unsign.rb
@@ -16,6 +16,14 @@ class Unsign < Formula
     end
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/stek29/idevice"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey: "dd7b71acb531c6bff145bc1357b93dd0e3693b0e86628726c5bbabe4dd2fe141"
+    sha256 cellar: :any_skip_relocation, big_sur:  "86f62038b2efa03f4c40e9686e7db035d4eac0a78665f5bd29ff3b8f0ba39dee"
+    sha256 cellar: :any_skip_relocation, catalina: "16896f3555c2352d623f382affee515dac8f388d56155352b59777f31485b8b5"
+  end
+
   def install
     system "make"
     bin.install "unsign"
