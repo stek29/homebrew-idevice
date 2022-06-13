b52a56c8ccd1872d6e5e408869b326d1c1bd64c0
Viktor Oreshkin <imselfish@stek29.rocks>
2022-06-13 05:42:40 +0300

    liboffsetfinder64: bump revision

--- Formula/liboffsetfinder64.rb
+++ Formula/liboffsetfinder64.rb
@@ -5,6 +5,7 @@ class Liboffsetfinder64 < Formula
     revision: "8d4fe0e495708995dce96445744654c89203f68a"
   version "152"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://github.com/Cryptiiiic/liboffsetfinder64.git"
 
   livecheck do
@@ -54,7 +55,7 @@ class Liboffsetfinder64 < Formula
 
   def caveats
     <<~EOS
-      this is not the "official" tihmstar/tsschecker, but a maintained fork by Cryptiiiic
+      this is not the "official" tihmstar/liboffsetfinder64, but a maintained fork by Cryptiiiic
     EOS
   end
 end
