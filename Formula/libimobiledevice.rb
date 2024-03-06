df16a64da8a26b056601b3fe00be5fd7f595387b
Viktor Oreshkin <imselfish@stek29.rocks>
2024-03-06 20:50:17 +0000

    libimobiledevice: update 1.3.0-210-g73b6fd1 bottle.

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -12,11 +12,9 @@ class Libimobiledevice < Formula
   end
 
   bottle do
-    root_url "https://ghcr.io/v2/stek29/idevice"
-    rebuild 1
-    sha256 cellar: :any, monterey: "7da19d95a57bb0cab25a791a561f07fd2e03b657114089add2e8f29f2bd11307"
-    sha256 cellar: :any, big_sur:  "0bd130bd38a83df2b47bd2cf6b194a9eace6c85692df297e7770f2be8035b28d"
-    sha256 cellar: :any, catalina: "408744b6dbb80fc7dd000c8a231205967407554189b075cd41139fd9b4680de1"
+    root_url "https://github.com/stek29/homebrew-idevice/releases/download/libimobiledevice-1.3.0-210-g73b6fd1"
+    sha256 cellar: :any, arm64_sonoma: "6d953628da0d7f806bcf5eba9682a17e6ebc87ae08ac08d0f5801759a4e95620"
+    sha256 cellar: :any, ventura:      "b4f790c84f3c435abad7b9ada2373a7a6f47e30dc12b9215293b90dea7cfcbf2"
   end
 
   keg_only "it can conflict with homebrew/core"
